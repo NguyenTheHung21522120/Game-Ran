8da3baab63157e10b08493a48ac532c7b7b22f40
UIT-21520255 <21520255@gm.uit.edu.vn>
2022-12-01 15:43:46 +0700

    update moi va khung

--- snake.cpp
+++ snake.cpp
@@ -10,6 +10,7 @@ using namespace std;
 #define WIDTH 40
 #define HEIGHT 20 
 #define BODY '*'
+#define APPLE "0"
 
 enum class Direction
 {
@@ -33,17 +34,19 @@ vector<Point> snake = {
 	Point{ WIDTH / 2 - 2, HEIGHT / 2 }
 };
 Direction direction = Direction::right;
+Point apple;
 Point prevTail;
-
+int score = 0;
 void drawSnakePart(Point);
 void drawSnake();
+void genApple();
 void gotoxy(int, int);
 void ShowConsoleCursor(bool);
 void move();
 void drawBox();
 void drawHeadnTail();
-
-
+bool isAteApple();
+void displayScore();
 void showEndMenu();
 void startGame();
 void resetSnake();
@@ -84,9 +87,9 @@ void startGame()
 {
 	system("cls");
 	ShowConsoleCursor(false);//An con tro chuot
-    drawBox();
+	drawBox();
 	drawSnake();//ve con ran
-
+	genApple();
 
 	while (true)//dinh nghia di chuyen ran
 	{
@@ -102,16 +105,19 @@ void startGame()
 				direction = Direction::down;
 			else if (ch == 'd' && direction != Direction::left)
 				direction = Direction::right;
-		
+
 		}
 		move();//di chuyen ran
-		
+
 		drawHeadnTail();//set lai ran moi
 		Sleep(1000);
-	
+		if (isAteApple())//An tao thi ran lon
+		{
+			score++;
+			displayScore();
+		}
 	}
 }
-
 
 void showStartMenu()
 {
@@ -125,7 +131,9 @@ void showStartMenu()
 			Sleep(1000);
 		}
 		gotoxy(0, 3);
+		cout << "Bat dau!";
 		Sleep(1000);
+
 		startGame();
 }
 void drawSnakePart(Point p)//ve 1 diem tren than ran
@@ -180,4 +188,28 @@ void ShowConsoleCursor(bool showFlag)// an con tro chuot
 	GetConsoleCursorInfo(out, &cursorInfo);
 	cursorInfo.bVisible = showFlag;
 	SetConsoleCursorInfo(out, &cursorInfo);
+}
+//them tao
+void genApple()
+{
+	srand(time(0));
+	int x = rand() % (WIDTH - 1) + 1;
+	int y = rand() % (HEIGHT - 1) + 1;
+	apple = {
+		x,
+		y,
+	};
+	gotoxy(x, y);
+	cout << "\033[1;31m" << APPLE << "\033[0m";
+}
+//An Tao
+bool isAteApple()
+{
+	return snake[0].x == apple.x && snake[0].y == apple.y;
+}
+//Tinh diem 
+void displayScore()
+{
+	gotoxy(WIDTH + 5, 2);
+	cout << "DIEM: " << score;
 }
